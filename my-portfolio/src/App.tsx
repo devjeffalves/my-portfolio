871478967835f78fe7264a45d49634c990fed697
devjeffalves <jeff-alves@outlook.com>
2024-02-27 16:45:41 -0300

    feat: adicionado botões

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -2,12 +2,14 @@ import Home from "./pages/Home/Home"
 
 
 
+
 const App = () => {
  
   return (
   <>
-  App
-  <Home />
+  
+  <Home/>
+  
   </>
 
   )
--- my-portfolio/src/pages/Home/Home.tsx
+++ my-portfolio/src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import Hero from "./sections/Hero/Hero"
 
 
 
@@ -5,7 +6,7 @@ const Home = () => {
  
     return (
     <>
-    Home
+  <Hero/>
     </>
   
     )
