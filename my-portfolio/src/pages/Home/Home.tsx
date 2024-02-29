2331d70d1054168788437abfb73cccca0899dbdc
devjeffalves <jeff-alves@outlook.com>
2024-02-29 11:22:06 -0300

    feat: adicionado animação e botões

--- my-portfolio/src/pages/Home/Home.tsx
+++ my-portfolio/src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import NavBar from "../../components/NavBar/NavBar"
 import Hero from "./sections/Hero/Hero"
 
 
@@ -6,6 +7,7 @@ const Home = () => {
  
     return (
     <>
+  <NavBar/>
   <Hero/>
     </>
   
--- my-portfolio/src/pages/Home/sections/Hero/Hero.tsx
+++ my-portfolio/src/pages/Home/sections/Hero/Hero.tsx
@@ -1,22 +1,35 @@
-import { Button, Container, Grid, Typography, styled } from "@mui/material"
+import { Box, Container, Grid, Typography, styled } from "@mui/material"
 import Avatar from "../../../../assets/images/avatar.png"
 import DownloadIcon from '@mui/icons-material/Download';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import StyledButton from "../../../../components/StyledButton/StyledButton";
+import theme from "../../../../theme";
+import { AnimatedBackground } from "../../../../components/AnimatedBackground/AnimatedBackground";
 
 
 
 
 const Hero = () => {
 
-    const StyledHero = styled ("div") (()=> ({
-        background: "black",
-        height: "100vh"
+    const StyledHero = styled ("div") (({theme})=> ({
+        background: theme.palette.primary.main,
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        [theme.breakpoints.up('xs')]: { // <= mobile
+            paddingTop: "100px",
+
+        },
+        [theme.breakpoints.up('md')]: { // >=mobile
+            paddingTop: "0",
+        }
 
     }))
 
     const StyledImage = styled ("img") (()=> ({
-        width: "100%",
-        borderRadius: "50%"
+        width: "75%",
+        borderRadius: "50%",
+        border: `1px solid ${theme.palette.primary.contrastText}`
 
     }))
  
@@ -26,23 +39,41 @@ const Hero = () => {
 
 <Container max-width ="lg">
 <Grid container spacing={2}>
-  <Grid item xs={12} md = {4}>
-  <StyledImage src={Avatar}/>
+  <Grid item xs={12} md = {5}>
+    <Box position = "relative">
+        <Box position = "absolute" width={"150%"} top={-100} right={0}>
+            <AnimatedBackground />
+        </Box>
+        <Box position="relative" textAlign="center">
+        <StyledImage src={Avatar}/>
+        </Box>
+
+
+    </Box>
+  
   </Grid>
-  <Grid item xs={12} md = {8}>
+  <Grid item xs={12} md = {7}>
 
-    <Typography color="primary" variant="h1" textAlign = "center">Jéferson Alves</Typography>
-    <Typography color="primary" variant="h2" textAlign = "center">I'm a Web Developer</Typography>
+    <Typography color="primary.contrastText" variant="h1" textAlign = "center">Jéferson Alves</Typography>
+    <Typography color="primary.contrastText" variant="h2" textAlign = "center" pb={2}>I'm a Web Developer</Typography>
    
-    <Grid container display={"flex"} justifyContent={"center"}>
+    <Grid container display={"flex"} justifyContent={"center"} spacing={3} pt={3}>
         <Grid item xs={12} md={4} display={"flex"} justifyContent={"center"} >
-            <Button> 
-        <DownloadIcon/>Download CV</Button>
+ <StyledButton onClick={() => console.log("Download")}>
 
+    <DownloadIcon/>
+    <Typography>Download CV</Typography>
+
+ </StyledButton>
+    
         </Grid>
         <Grid item xs={12} md={4} display={"flex"} justifyContent={"center"} >
-        <Button>
-        <MailOutlineIcon/>Contact me</Button>
+       
+        <StyledButton onClick={() => console.log("Contact me")}>
+        <MailOutlineIcon/>
+        <Typography>Contact me</Typography>
+        </StyledButton>
+        
         </Grid>
         
     </Grid>
